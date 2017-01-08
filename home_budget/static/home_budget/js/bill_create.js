c9a61b584fdda88449e1a6c00174b63d716e0035
Jakub Nurski <nurskijakub@gmail.com>
2017-01-08 13:30:22 +0100

    Added Bootstrap glyph icons. Tweaked site with a bit of CSS. Datepicker configured using JS.

--- home_budget/static/home_budget/js/bill_create.js
+++ home_budget/static/home_budget/js/bill_create.js
@@ -1,6 +1,18 @@
 (function($){
-    var brandSelect = jQuery('#id_brand')[0];
-    var shopSelect = jQuery('#id_shop_address')[0];
+    $('#bill-date').datepicker({
+        format: "dd/mm/yyyy",
+        weekStart: 1,
+        todayBtn: "linked",
+        language: "pl",
+        keyboardNavigation: false,
+        todayHighlight: true
+    });
+    $('#bill-date').datepicker('update', new Date());
+
+    var createBillForm = $('#create-bill-form');
+    var brandSelect = $('#id_brand')[0];
+    var shopSelect = $('#id_shop_address')[0];
+    var disabledInputs = $(createBillForm).find('select, input').slice(2);
 
     var onBrandSelectChange = function() {
         var brand = brandSelect.value;
@@ -11,10 +23,10 @@
             shopSelect.append(new Option(shop, shop));
         });
         if(brandShops != undefined) {
-            $(shopSelect).prop('disabled', '');
+            $(disabledInputs).prop('disabled', '');
         } else {
             shopSelect.append(new Option("BRAK SKLEPU!", ""));
-            $(shopSelect).prop('disabled', 'disabled');
+            $(disabledInputs).prop('disabled', 'disabled');
         }
     }
 
