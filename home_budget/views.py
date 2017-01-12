4707ce982c8090ec276f73dd6e4ab9fc2fbf8f83
Jakub Nurski <nurskijakub@gmail.com>
2017-01-12 13:43:13 +0100

    Refactorized views.py a bit.

--- home_budget/views.py
+++ home_budget/views.py
@@ -16,61 +16,42 @@ class BillCreateView(TemplateView):
 
     template_name = "bill_create.html"
 
-    def get_shops(self):
-        shops = Sklepy.objects.all().values('sieci_sklepow_nazwa', 'adres').order_by('sieci_sklepow_nazwa', 'adres')
+    def get(self, request, instance=None, Formset=PurchaseFormSet,
+        *args, **kwargs):
+        bill_form = BillForm(data=None, instance=instance)
 
-        brands_shops = defaultdict(list)
-        for shop in shops:
-            brand = shop['sieci_sklepow_nazwa']
-            brands_shops[brand].append(shop['adres'])
+        purchase_formset = Formset(data=None, instance=instance)
 
-        return json.dumps(brands_shops)
-
-    def get(self, request, *args, **kwargs):
-        bill_form = BillForm()
-
-        purchase_formset = PurchaseFormSet()
-
-        context = {
-            'form': bill_form,
-            'purchase_formset': purchase_formset,
-            'shops': self.get_shops(),
-        }
-
-        return self.render_to_response(context)
+        return self.render_context(bill_form, purchase_formset)
 
     def post(self, request, *args, **kwargs):
         bill_form = BillForm(data=request.POST)
 
-        formset = PurchaseFormSet(data=request.POST)
+        purchase_formset = PurchaseFormSet(data=request.POST)
 
-        print("POST")
-        print(request.POST)
-        print("Bill: ", bill_form)
-        print("Is valid: ", bill_form.is_valid())
-        print("Purchases: ", formset)
-        print("Is valid: ", formset.is_valid())
-
-        if bill_form.is_valid() and formset.is_valid():
+        if bill_form.is_valid() and purchase_formset.is_valid():
             bill = bill_form.save()
-            purchases = formset.save(commit=False)
+            purchases = purchase_formset.save(commit=False)
             for purchase in purchases:
                 purchase.paragony = bill
                 purchase.save()
             return HttpResponseRedirect(reverse('bill_create'))
 
+        return self.render_context(bill_form, purchase_formset)
+
+    def render_context(self, bill_form, purchase_formset):
+        print(bill_form)
+        print(purchase_formset)
+
         context = {
             'form': bill_form,
-            'formset': formset,
-            'shops': self.get_shops(),
+            'purchase_formset': purchase_formset,
+            'shops': self._get_shops(),
         }
+
         return self.render_to_response(context)
 
-class BillDetailsView(TemplateView):
-
-    template_name = "bill_create.html"
-
-    def get_shops(self):
+    def _get_shops(self):
         shops = Sklepy.objects.all().values('sieci_sklepow_nazwa', 'adres').order_by('sieci_sklepow_nazwa', 'adres')
 
         brands_shops = defaultdict(list)
@@ -80,19 +61,13 @@ class BillDetailsView(TemplateView):
 
         return json.dumps(brands_shops)
 
+
+class BillDetailsView(BillCreateView):
+
+    template_name = "bill_details.html"
+
     def get(self, request, *args, **kwargs):
         pk = self.kwargs['pk']
         bill = Paragony.objects.get(id=pk)
-        bill_form = BillForm(instance=bill)
 
-        purchases = Zakupy.objects.filter(paragony=pk)
-
-        purchase_formset = PurchaseRetrieveFormSet(instance=bill)
-
-        context = {
-            'form': bill_form,
-            'purchase_formset': purchase_formset,
-            'shops': self.get_shops(),
-        }
-
-        return self.render_to_response(context)
+        return super(BillDetailsView, self).get(request, bill, PurchaseRetrieveFormSet, *args, **kwargs)
