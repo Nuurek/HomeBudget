69549270da42e8d1e15670e4b91a6f59b14a7df8
Jakub Nurski <nurskijakub@gmail.com>
2017-01-15 00:35:55 +0100

    Prepared JS file for brand details view.

---
+++ home_budget/static/home_budget/js/brand.js
@@ -0,0 +1,22 @@
+function setUpBrandName() {
+    var brandName = $('#brand-name');
+    var editButton = $('#brand-name-edit');
+    var icon = $(editButton.find('span'));
+
+    editButton.click(function(){
+        editButton.blur();
+        icon.removeAttr('class');
+
+        if (brandName.attr('disabled')) {
+            brandName.removeAttr('disabled');
+            icon.attr('class', 'glyphicon glyphicon-ok');
+        } else {
+            brandName.attr('disabled', '');
+            icon.attr('class', 'glyphicon glyphicon-edit');
+        }
+    });
+}
+
+(function($){
+    setUpBrandName();
+})(jQuery);
